db1e747d57ef1fb1adc62d17bb936f4ad8888dd9
Guilherme Viana <97542638+GuilhermeMViana@users.noreply.github.com>
2023-02-15 10:02:45 -0300

    Removendo dados do localStorage

--- js/main.js
+++ js/main.js
@@ -12,16 +12,28 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
+    const verificarItem = itens.find(elemento => elemento.nome === nome.value)
+
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
 
-    itens.push(itemAtual)
+    if (verificarItem) {
+        itemAtual.id = verificarItem.id
+
+        atualizaElemento(itemAtual)
+
+        itens[itens.findIndex(elemento => elemento.id === verificarItem.id)] = itemAtual
+    } else {
+        itemAtual.id = itens[itens.length - 1] ? (itens[itens.length - 1]).id + 1 : 0
+
+        criarElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
 
     localStorage.setItem("itens", JSON.stringify(itens))
-
-    criarElemento(itemAtual)
 
     nome.value = ""
     quantidade.value = ""
@@ -33,11 +45,37 @@ function criarElemento(item) {
 
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = item.quantidade
-
+    numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem)
     novoItem.innerHTML += item.nome
+
+    novoItem.appendChild(botaoDelete(item.id))
 
     lista.appendChild(novoItem)
 
 }
 
+function atualizaElemento(item) {
+    document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade
+}
+
+function botaoDelete(id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function () {
+        deletar(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletar(tag, id) {
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+
+}
